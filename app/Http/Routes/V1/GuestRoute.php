676c0efe0a3bfc860fb282c5870750bda69f8b3b
lucyjack886 <lucyjack886@users.noreply.github.com>
2025-09-28 11:30:28 +0800

    test

--- app/Http/Routes/V1/GuestRoute.php
+++ app/Http/Routes/V1/GuestRoute.php
@@ -16,6 +16,8 @@ class GuestRoute
             $router->match(['get', 'post'], '/payment/notify/{method}/{uuid}', 'V1\\Guest\\PaymentController@notify');
             // Comm
             $router->get ('/comm/config', 'V1\\Guest\\CommController@config');
+            // Plan - 公开查询计划
+            $router->get('/plan/fetch', 'V1\\Guest\\PlanController@fetch');
         });
     }
 }
