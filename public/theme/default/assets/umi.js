a8806467c78cff44805a62d254d045a8acfb109f
lucyjack886 <lucyjack886@users.noreply.github.com>
2025-10-10 18:07:15 +0800

    commit

--- public/theme/default/assets/umi.js
+++ public/theme/default/assets/umi.js
@@ -57493,7 +57493,7 @@
                                     if (d && d.data && d.data.auth_data) {
                                         Object(c["p"])(d.data.auth_data);
                                     }
-                                    s.a.push("dashboard");
+                                    s.a.push("/dashboard");
                                 case 14:
                                 case "end":
                                     return n.stop()
